aae2c3a452ee104abcbd90181a4370a4e108b803
sebastien544 <59696100+sebastien544@users.noreply.github.com>
2023-04-17 20:32:01 +0700

    Update search.js

--- search.js
+++ search.js
@@ -183,17 +183,17 @@ function displayResults(results) {
   resultList.style.cssText = "box-shadow: 0 0 0 1px rgb(35 38 59 / 10%), 0 6px 16px -4px rgb(35 38 59 / 15%); border-radius: 4px; padding: 8px;background: #fff; ";
   resultList.style.background = 'white';
   
- if (window.matchMedia("(max-width: 700px)").matches) {
+ 
    document.querySelector('#elastic-search').appendChild(resultList);
- } else {
-   resultList.style.position = "absolute";
+
+   /*resultList.style.position = "absolute";
    const inputRect = document.querySelector('form');
   resultList.style.width = inputRect.offsetWidth + "px";
   resultList.style.left = inputRect.getBoundingClientRect().left + 'px';
   resultList.style.top = (inputRect.getBoundingClientRect().bottom + 10) + 'px';
   
  
-  document.querySelector('body').appendChild(resultList);
+  document.querySelector('body').appendChild(resultList);*/
  }
 
   
